ebe05a83e49ceb1943aeada1110a0b61f9ed4c6e
Shaxzodbek <shaxzodbek.qambaraliyev@gmail.com>
2022-05-28 19:16:23 +0500

    laravel test

--- app/Models/User.php
+++ app/Models/User.php
@@ -12,6 +12,7 @@ use Laravel\Sanctum\HasApiTokens;
 
 class User extends Authenticatable
 {
+    
     use HasApiTokens;
     use HasFactory;
     use HasProfilePhoto;
--- tests/Feature/UserTest.php
+++ tests/Feature/UserTest.php
@@ -2,21 +2,48 @@
 
 namespace Tests\Feature;
 
+use App\Models\User;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Testing\Fluent\AssertableJson;
 use Tests\TestCase;
 
 class UserTest extends TestCase
 {
+    use RefreshDatabase;
     /**
      * A basic feature test example.
      *
      * @return void
      */
-    public function test_users_list_status()
+    public function test_home_page()
     {
-        // factory(User::class, 10)->create();
-        $response = $this->get('/users');
+        $response = $this->get('/');
         $response->assertStatus(200);
+    }
+
+    public function test_users_list_in_api()
+    {
+        $response = $this->getJson('/api/users');
+        $response->assertJson(fn (AssertableJson $json) =>
+            $json->hasAll(['success', 'data'])
+        );
+    }
+    public function test_users_list_count_in_api()
+    {
+        $count = User::count();
+        $response = $this->getJson('/api/users');
+        $response->assertJson(fn (AssertableJson $json) =>
+            $json->has('data')
+                ->has('data', $count)
+                ->etc()
+            
+        );
+    }
+
+    public function test_count_users()
+    {
+        User::factory()->count(10)->create();
+        $this->assertDatabaseCount('users', 10);
     }
 }
